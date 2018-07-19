5adcef50c1e9da76b64a3344b04da92a91c24edc
DR-FRONT-PC\Anthonyh <ahowe2@att.net>
2018-07-19 14:30:32 -0400

    2nd commit

---
+++ Source/HowTo_Components/CollidingPawnMovementComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CollidingPawnMovementComponent.h"
+
+
+
+
+
+void UCollidingPawnMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!PawnOwner || !UpdatedComponent || ShouldSkipUpdate(DeltaTime))
+	{
+		return;
+	}
+	FVector DesiredMovementThisFrame = ConsumeInputVector().GetClampedToMaxSize(1.f) * DeltaTime * 150.f;
+	if (!DesiredMovementThisFrame.IsNearlyZero())
+	{
+		FHitResult Hit;
+		SafeMoveUpdatedComponent(DesiredMovementThisFrame, UpdatedComponent->GetComponentRotation(), true, Hit);
+
+		if (Hit.IsValidBlockingHit())
+		{
+			SlideAlongSurface(DesiredMovementThisFrame, 1.f - Hit.Time, Hit.Normal, Hit);
+		}
+	}
+
+}
